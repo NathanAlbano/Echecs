d5b9d4f346a79e3d449939429aa7c0226018cfb0
MatthieuChapeu <cocbbmg@gmail.com>
2025-01-29 14:22:09 +0100

    First chessboard version

--- Chess_Game/classes/Chessboard.py
+++ Chess_Game/classes/Chessboard.py
@@ -3,5 +3,31 @@ Author(s): ...
 Creation date: 12/18/2024
 Last modification: 12/18/2024
 
-Description: A python class introducing a chessboard mathematical representation 
+Description: A python class introducing a chessboard's mathematical representation 
 """
+import numpy as np
+
+class Chessboard:
+    '''
+    No parameters to build the chessboard.
+    The chessboard is represented by an integer array. Each piece has a different numero, 0 meaning that no pieces is 
+    on a square. You can find the different pieces' numero in ./Pieces.py
+    '''
+    def __init__(self):
+        '''
+        Builds a 8*8 chessboard with 0 (no pieces)
+        '''
+        self.chessboard = np.zeros((8,8))
+
+        column_to_letter = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
+        self.index_to_square = {}
+        for i in range(8):
+            for j in range(8):
+                self.index_to_square[(i,j)] = f"{column_to_letter[i]}{j+1}"
+
+    def print_chessboard(self):
+        print(self.chessboard)
+
+## === TEST SECTION === ##
+test = Chessboard()
+print(test.index_to_square[(7,7)])
